25786748d0a19752e608eba80e9f22b2e44f4d8b
Adam Goldfine <tulaneadam21@gmail.com>
2023-06-09 17:55:14 -0400

    .

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Speech from 'startsys-react-speech-recognition';
+import { FaMicrophone } from 'react-icons/fa';
+
+function App() {
+  const [transcript, setTranscript] = useState("");
+  
+  const handleSpeech = (command) => {
+    setTranscript(command);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Speech Recognition App</h1>
+        <div className="input-group">
+          <Speech onSpeech={handleSpeech}>
+            <FaMicrophone className="fas fa-2x" />
+          </Speech>
+          <input type="text" value={transcript} readOnly />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
